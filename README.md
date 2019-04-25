5572837ed390d962d048dbb73160af1fedaa6611
Shwise <thesecondwiseman@gmail.com>
2019-04-25 11:51:21 -0400

    New url, fresh start

---
+++ README.md
@@ -0,0 +1,14 @@
+# Breviary Website
+> A PWA containing the 1962 Divine Office
+
+## Purpose
+
+To make the 1962 Divine Office more accessible, through a largely-static progressive web application.
+
+## Sources
+
+All of the contained text comes from the [Divinum Officium project](http://www.divinumofficium.com/www/horas/Help/credits.html). A big thank you to all their contributors.
+
+## Feature Requests
+
+Please feel free to request features via the issues tab, however I cannot promise all will be incorporated.
---
+++ service-worker.js
@@ -0,0 +1,63 @@
+const filesToCache = [
+   'index.html',
+   'error.html',
+   'style.css',
+   'Styles/s.css',
+   'Text/s.css',
+   'favicon-32x32.png',
+   'favicon-16x16.png'
+];
+
+var brevCache = 'pages-cache';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(brevCache)
+    .then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+
+  const cacheWhitelist = [brevCache];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request)
+
+      .then(response => {
+        if (response.status === 404) {
+          return caches.match('error.html');
+        }
+        return caches.open(brevCache).then(cache => {
+          cache.put(event.request.url, response.clone());
+          return response;
+        });
+      });
+
+    }).catch(error => {
+      return caches.match('error.html');
+
+    })
+  );
+});
