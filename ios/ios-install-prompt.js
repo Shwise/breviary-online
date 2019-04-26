ebb7cd4712f275f834180b0bd272f05297691cc4
Shwise <thesecondwiseman@gmail.com>
2019-04-25 21:47:32 -0400

    Large style updates, and iOS install prompt

---
+++ ios/ios-install-prompt.js
@@ -0,0 +1,18 @@
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+}
+
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+if (isIos() && !isInStandaloneMode()) {
+  launch_prompt()
+}
+
+function launch_prompt() {
+  let iOSpopup = document.querySelector(".ios-popup");
+  iOSpopup.style.display = "table";
+  iOSpopup.addEventListener("click", () => {
+    iOSpopup.style.display = "none";
+  });
+}
